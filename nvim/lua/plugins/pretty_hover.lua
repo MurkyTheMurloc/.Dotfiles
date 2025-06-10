bbd487e98e1cab9b7fd2304d29b6d6b743e55b5b
Paul <72733450+paul1995tu@users.noreply.github.com>
2025-06-10 13:11:19 +0200

    nothing works

---
+++ nvim/lua/plugins/pretty_hover.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        "Fildo7525/pretty_hover",
+        event = "LspAttach",
+        opts = {
+            border       = "rounded",
+            wrap         = true,
+            max_width    = 70,
+            max_height   = 20,
+            toggle       = false,
+            multi_server = true,
+        }
+    },
+}
